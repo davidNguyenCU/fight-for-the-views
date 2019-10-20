c511ddb0c206ed4d94d5e043800f8474f3f81723
Petalousa <terrapetalousa@gmail.com>
2019-10-20 16:19:29 -0400

    merged David's code into mine

---
+++ Assets/hitZone.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class hitZone : MonoBehaviour
+{
+    //Make sure to assign this in the Inspector window
+    Collider m_Collider;
+    Vector3 m_Point;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Fetch the Collider from the GameObject this script is attached to
+        m_Collider = GetComponent<Collider>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if ((other.GetComponent<Rigidbody>().position.y <= 0)
+                && (other.GetComponent<Rigidbody>().position.y > -12)
+                && (other.GetComponent<arrow>().direction == "down"))
+            {
+                print("Hit!");
+                GameManager.comboCount++;
+                GameManager.changePhase();
+                other.GetComponent<arrow>().despawn(false);
+            }
+            else
+            {
+                if (other.GetComponent<arrow>().direction == "down")
+                {
+                    print("Partial Hit");
+                    GameManager.comboCount++;
+                    GameManager.changePhase();
+                }
+                else
+                {
+                    GameManager.comboCount = -1;
+                    GameManager.changePhase();
+                }
+                    
+                other.GetComponent<arrow>().despawn(false);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if ((other.GetComponent<Rigidbody>().position.y <= 0)
+                && (other.GetComponent<Rigidbody>().position.y > -12)
+                && (other.GetComponent<arrow>().direction == "up"))
+            {
+                print("Hit!");
+                GameManager.comboCount++;
+                GameManager.changePhase();
+                other.GetComponent<arrow>().despawn(false);
+            }
+            else
+            {
+                if (other.GetComponent<arrow>().direction == "up")
+                {
+                    print("Partial Hit");
+                    GameManager.comboCount++;
+                    GameManager.changePhase();
+                }
+                else
+                {
+                    GameManager.comboCount = -1;
+                    GameManager.changePhase();
+                }
+                other.GetComponent<arrow>().despawn(false);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if ((other.GetComponent<Rigidbody>().position.y <= 0)
+                && (other.GetComponent<Rigidbody>().position.y > -12)
+                && (other.GetComponent<arrow>().direction == "left"))
+            {
+                print("Hit!");
+                GameManager.comboCount++;
+                GameManager.changePhase();
+                other.GetComponent<arrow>().despawn(false);
+
+            }
+            else
+            {
+                if (other.GetComponent<arrow>().direction == "left")
+                {
+                    print("Partial Hit");
+                    GameManager.comboCount++;
+                    GameManager.changePhase();
+                }
+                else
+                {
+                    GameManager.comboCount = -1;
+                    GameManager.changePhase();
+                }
+                other.GetComponent<arrow>().despawn(false);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if ((other.GetComponent<Rigidbody>().position.y <= 0)
+                && (other.GetComponent<Rigidbody>().position.y > -12)
+                && (other.GetComponent<arrow>().direction == "right"))
+            {
+                print("Hit!");
+                GameManager.comboCount++;
+                GameManager.changePhase();
+                other.GetComponent<arrow>().despawn(false);
+
+            }
+            else
+            {
+                if (other.GetComponent<arrow>().direction == "right")
+                {
+                    print("Partial Hit");
+                    GameManager.comboCount++;
+                    GameManager.changePhase();
+                }
+                else
+                {
+                    GameManager.comboCount = -1;
+                    GameManager.changePhase();
+                }
+                other.GetComponent<arrow>().despawn(false);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GameManager.comboCount = -1;
+        GameManager.changePhase();
+        other.GetComponent<arrow>().despawn(true);
+    }
+}
