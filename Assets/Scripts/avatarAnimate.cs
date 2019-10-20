20afc20748840590c4a1d00bde8fdc33bb89aef8
Petalousa <moonmanedm@gmail.com>
2019-10-20 12:02:07 -0400

    added hurt, ko, idle, and puch animations. Added more transitions, and randomized punch types to avatar.

--- Assets/Scripts/avatarAnimate.cs
+++ Assets/Scripts/avatarAnimate.cs
@@ -6,25 +6,33 @@ using UnityEngine;
 public class avatarAnimate : MonoBehaviour
 {
     Animator anim;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
 
-        //anim.Play("PunchLeft");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.95){
-            anim.SetTrigger("AnimEnd");
+        if(Input.GetKeyDown(KeyCode.W)){
+            anim.SetTrigger("Hurt");
         }
-        //anim.Play("PunchLeft");
         
-        //anim.Play("PunchRight");
-        
+        if(Input.GetKeyDown(KeyCode.A)){
+            anim.SetTrigger("KO");
+        }
 
+        if(Input.GetKeyDown(KeyCode.S)){
+            anim.SetTrigger("Punch");
+        }
+        
+        anim.SetInteger("PunchTransition", (anim.GetInteger("PunchTransition") + 1) % 3);        
+//        if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.99){
+  //          anim.SetTrigger("AnimEnd");
+    //    }
     }
 }
