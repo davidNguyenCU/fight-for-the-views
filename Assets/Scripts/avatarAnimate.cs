ee975cc3c78ad10019bcb24626bed01ad108d903
Petalousa <moonmanedm@gmail.com>
2019-10-20 11:21:02 -0400

    added boxer animations. added animation to boxer in main scene

---
+++ Assets/Scripts/avatarAnimate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class avatarAnimate : MonoBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        //anim.Play("PunchLeft");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.95){
+            anim.SetTrigger("AnimEnd");
+        }
+        //anim.Play("PunchLeft");
+        
+        //anim.Play("PunchRight");
+        
+
+    }
+}
